e95002b043e9d997012862ac4b54155b2b2b5952
Virtual-Machine <darrelm.mailloux@gmail.com>
2023-12-25 12:16:52 -0500

    0.0.1 - Document result_error

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,22 @@
+//! demo macro lib to generate special result error structs
+
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
+
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use std::collections::HashMap;
 
 #[proc_macro]
+/// takes tokenstream input to generate result error token stream
+///
+/// # Arguments
+///
+/// * `input` - provided token stream
+///
+/// # Returns
+///
+/// generated tokenstream of ResultError struct
 pub fn make_result_error(input: TokenStream) -> TokenStream {
     let input_str = input.to_string();
 
@@ -13,6 +27,15 @@ pub fn make_result_error(input: TokenStream) -> TokenStream {
     to_stream(input_str, err_match, impl_froms)
 }
 
+/// takes input string to generate HashMap of token strings
+///
+/// # Arguments
+///
+/// * `input_str` - provided token stream as string
+///
+/// # Returns
+///
+/// generated hashmap of token strings
 fn idents_from_input(input_str: String) -> HashMap<String, String> {
     let mut idents = HashMap::new();
     let parts = input_str.split(", ");
@@ -25,6 +48,15 @@ fn idents_from_input(input_str: String) -> HashMap<String, String> {
     idents
 }
 
+/// generate component strings from hashmap values
+///
+/// # Arguments
+///
+/// * `idents` - provided hashmap identifier token strings
+///
+/// # Returns
+///
+/// err_match and impl_froms component strings
 fn gen_components(idents: HashMap<String, String>) -> (String, String) {
     let mut err_match = "".to_owned();
     let mut impl_froms = "".to_owned();
@@ -45,6 +77,17 @@ fn gen_components(idents: HashMap<String, String>) -> (String, String) {
     (err_match, impl_froms)
 }
 
+/// generate token stream from `input_str`, `err_match`, `impl_froms`
+///
+/// # Arguments
+///
+/// * `input_str` - provided token stream as string
+/// * `err_match` - err_match component string
+/// * `impl_froms` - impl_froms component string
+///
+/// # Returns
+///
+/// final token stream for ResultError macro
 fn to_stream(input_str: String, err_match: String, impl_froms: String) -> TokenStream {
     format!(
         "#[derive(Debug)]
